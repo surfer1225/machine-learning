56bef424c8f3720a4f79db2a5f39ede4825da274
Yao Wenchi <wenchi_yao@yahoo.com>
2017-07-11 17:39:07 +0800

    ex2: Regularised functions done

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -7,20 +7,17 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
+J = ((-y') * log(h) - (1 - y)' * log(1 - h))/m + lambda / (2 * m) * (sum(theta.^2) - theta(1,1)^2);
 
-
-
-
+theta(1,1) = 0;
+grad = 1/m * (X' * (h - y)) + lambda/m * theta;
 
 % =============================================================
 
